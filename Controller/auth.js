2c048da096d4520ad663c55a2bb92cbe48440b90
Ryan Montgomery <ryan.maciomaire@gmail.com>
2018-08-07 11:20:46 -0500

    prevent registration with existing user

--- Controller/auth.js
+++ Controller/auth.js
@@ -9,17 +9,12 @@ const getZipByIp = require('../IPStack/Geolocate')
 const Users = require('../Models/User')
 
 const cookieLength = process.env.COOKIELENGTH||24*60*60*1000
-const cookieOptions = process.env.COOKIEOPTIONS||{ maxAge: cookieLength, httpOnly: false, sameSite:true }
+const cookieOptions = process.env.COOKIEOPTIONS||{ maxAge: cookieLength, httpOnly: false, sameSite:'strict' }
 
-router.post('/register', async (req, res, next)=>{
-    let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    console.log(chalk.grey(`(${ip})`)+` Create request received for user: ${req.body.username}`);
+const getUsers = async () => { return await Users.find({}) }
+const deleteAllUsers = async () => { for (user of await getUsers()) Users.findByIdAndRemove(user.id) }
 
-    let newUser = req.body, hashPass = await bcrypt.hash(req.body.password, 10);
-    newUser.password = hashPass; newUser.username = newUser.username.replace(/[^a-z0-9]/gi,'');
-    let result = getZipByIp(ip)
-
-    Object.assign(newUser,await result);
+const createUser = async (newUser, req, res) =>{
 
     try{
         let createdUser = await Users.create(newUser);
@@ -32,7 +27,8 @@ router.post('/register', async (req, res, next)=>{
         if (createdUser.city) res.cookie('city', createdUser.city, cookieOptions)
         res.json({
             status: 201,
-            data: 'Registration successful'
+            data: 'Registration successful',
+            user: createdUser
         });
     } catch (err) { 
         console.error(err);
@@ -41,13 +37,36 @@ router.post('/register', async (req, res, next)=>{
             data: err,
         });
     }
-});
+}
+
+router.post('/register', async (req, res, next)=>{
+    let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    console.log(chalk.grey(`(${ip})`)+` Create request received for user: ${req.body.username}`);
+
+    let newUser = req.body, hashPass = await bcrypt.hash(req.body.password, 10);
+    newUser.password = hashPass; newUser.username = newUser.username.replace(/[^a-z0-9]/gi,'');
+    let result = getZipByIp(ip)
+
+    Object.assign(newUser,await result);
+
+    try {
+        let foundUser = await Users.find({username: newUser.username})
+        console.error(chalk.red('Could not create user. Username taken: ')+chalk.grey(newUser.username))
+        res.json({
+            status: 409,
+            data: 'Username in use',
+            });
+    } catch (err){
+        console.log(err)
+        createUser(newUser, req, res);
+    }
+ });
 
 router.post('/login', async (req, res, next) =>{
     let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
     console.log(chalk.grey(`(${ip})`)+` Attempting to login user: ${req.body.username}`);
     try{
-        let foundUser = await Users.findOne({username:req.body.username}, ()=>{
+        let foundUser = await Users.find({username:req.body.username}, ()=>{
             console.log(chalk.red(`Could not find username: `)+chalk.grey(req.body.username));
         })
         if (foundUser!=undefined){
@@ -64,6 +83,7 @@ router.post('/login', async (req, res, next) =>{
                 res.json({
                     status: 200,
                     data: 'Login successful',
+                    user: foundUser
                 });
             } else {
                 console.log(chalk.red('Invalid password'))
@@ -74,6 +94,7 @@ router.post('/login', async (req, res, next) =>{
             }
         }
      } catch(err){ 
+                console.error(err)
                 res.json({
                     status: 409,
                     data: 'Invalid username',
